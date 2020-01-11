8837c3c31f09b0bb62e8f12293069fe53fc558bf
mob41 <mob41@users.noreply.github.com>
2020-01-12 01:16:55 +0800

    Initial commit

---
+++ src/js/components/GameModal.js
@@ -0,0 +1,48 @@
+﻿import React from 'react';
+import { Modal, Button, ButtonGroup, InputGroup, FormControl, Form } from 'react-bootstrap';
+
+export default function(props) {
+    return (
+        <Modal show={props.show} animation={false} backdrop="static">
+            <Modal.Header>
+                <Modal.Title>Play³</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <label>Join a party:</label>
+                <InputGroup className="mb-3">
+                    <FormControl
+                        placeholder="Party Code"
+                        aria-label="Party Code"
+                        aria-describedby="party-code-field-addon"
+                    />
+                    <InputGroup.Append>
+                        <Button variant="success">Join</Button>
+                    </InputGroup.Append>
+                </InputGroup>
+                <div className="d-flex">
+                    <Button variant="success" className="btn-block">Quick join</Button>
+                    <Button variant="secondary" className="ml-2"><i className="fas fa-cog"></i></Button>
+                </div>
+                <hr />
+                <p>Start a new game:</p>
+                <div className="hori-scroll">
+                    <Button variant="secondary" onClick={() => { props.startGame("classic") }}><i className="fas fa-heart fa-2x"></i><br />Classic</Button>
+                    <Button variant="secondary" onClick={() => { props.startGame("royal") }}><i className="fas fa-grin-stars fa-2x"></i><br />Royal</Button>
+                    <Button variant="secondary" onClick={() => { props.startGame("twentyone") }}><i className="fas fa-sort-numeric-down fa-2x"></i><br />Twenty-one</Button>
+                    <Button variant="secondary" onClick={() => { props.startGame("guessnum") }}><i className="fas fa-list-ol fa-2x"></i><br />Guess Number</Button>
+                    <Button variant="secondary" onClick={() => { props.startGame("guessbig") }}><i className="fas fa-dice fa-2x"></i><br />Guess Big</Button>
+                </div>
+                <Form.Check
+                    type="switch"
+                    id="local-mode-switch"
+                    label="Start game locally"
+                    checked={true}
+                    disabled
+                />
+            </Modal.Body>
+            <Modal.Footer>
+                <p>Application licensed under the GPLv3 Public License.</p>
+            </Modal.Footer>
+        </Modal>
+    );
+}
---
+++ src/js/components/Playground.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import SetupLocalPlayersModal from './SetupLocalPlayersModal';
+import PassToPlayerModal from './PassToPlayerModal';
+import { Spinner } from 'react-bootstrap';
+
+export default class Playground extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.players = false;
+        this.turns = 0;
+        this.state = { loading: true, loadingMsg: "Loading playground" };
+    }
+
+    componentDidMount() {
+        if (this.props.localMode) {
+            this.setupLocalPlayers(this.getMinimumPlayers(), this.getMaximumPlayers());
+        } else {
+            this.enable();
+        }
+    }
+
+    componentWillUnmount() {
+        this.disable();
+    }
+
+    isSetupComplete() {
+        return this.props.localMode && this.players;
+    }
+
+    setupLocalPlayers(min, max) {
+        this.setState({
+            loadingMsg: "Setting up local players",
+            minPlayers: min,
+            maxPlayers: max,
+            setupLocalPlayers: true
+        });
+    }
+
+    startGame() {
+
+    }
+
+    ready() {
+        if (this.props.localMode) {
+            this.startGame();
+        }
+        //TODO Submit ready status
+    }
+
+    readyNextTurn() {
+        if (this.props.localMode) {
+            if (this.players.length > 1) {
+                var player = this.players[this.turns++ % this.players.length];
+                this.setState({ waitPlayer: true, playerName: player });
+            } else {
+                this.nextTurn();
+            }
+        } else {
+            //TODO online mode check
+        }
+    }
+
+    leave() {
+        this.disable();
+        this.props.onDisable();
+        //TODO Submit leave status
+    }
+
+    getMinimumPlayers() {
+        return 1;
+    }
+
+    getMaximumPlayers() {
+        return 1;
+    }
+
+    nextTurn() {
+
+    }
+
+    enable() {
+
+    }
+
+    disable() {
+
+    }
+
+    renderPlayground() {
+
+    }
+
+    render() {
+        var playersList = false;
+        if (this.players) {
+            playersList = [];
+            var i;
+            for (i = 0; i < this.players.length; i++) {
+                playersList.push(<div key={this.players[i]}><span className={this.state.playerName === this.players[i] ? "font-weight-bold" : ""}>{this.players[i]}</span><br /></div>);
+            }
+        }
+        return (
+            <div className="container-fluid playground">
+                {this.state.loading &&
+                    <div className="loading d-flex align-items-center justify-content-center">
+                    <center><Spinner animation="border" /><br /><span>{this.state.loadingMsg}</span></center>
+                    </div>
+                }
+                {this.isSetupComplete() && this.renderPlayground()}
+                {this.state.setupLocalPlayers &&
+                    <SetupLocalPlayersModal min={this.state.minPlayers} max={this.state.maxPlayers} show={this.state.setupLocalPlayers} onComplete={
+                    (map) => {
+                        console.log(map);
+                        this.players = map;
+                        this.setState({
+                            setupLocalPlayers: false,
+                            loadingMsg: "Loading playground"
+                        });
+                        this.enable();
+                    }} />
+                }
+                {this.state.waitPlayer &&
+                    <PassToPlayerModal show={this.state.waitPlayer} playerName={this.state.playerName} onContinue={(() => {
+                        this.setState({ waitPlayer: false });
+                        this.nextTurn();
+                    }).bind(this)} />
+                }
+                {playersList &&
+                    <div className="party-list">
+                        <h4>Party</h4>
+                        {playersList}
+                    </div>
+                }
+            </div>
+        );
+    }
+};
