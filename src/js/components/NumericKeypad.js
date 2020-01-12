08cecc2bd21134c6dcab1d1e8926ff373e50d9c4
Anthony Law <puihuenlaw@gmail.com>
2020-01-12 12:24:10 +0800

    Add green screen

---
+++ src/js/components/NumericKeypad.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ButtonGroup, Button } from 'react-bootstrap';
+
+export default function () {
+    return (
+        <div className="container-fluid touch-keypad row">
+
+        </div>
+    );
+}
