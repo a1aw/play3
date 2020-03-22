adf84f7c3fe9d221ad9e83ffed4e7c1f6274a1c2
mob41 <mob41@users.noreply.github.com>
2020-03-22 17:02:19 +0800

    Add special AI

--- src/js/lib/games/bigtwo/sizes.js
+++ src/js/lib/games/bigtwo/sizes.js
@@ -2,5 +2,6 @@ module.exports = {
     SUIT: ["diamonds", "clubs", "hearts", "spades"],
     RANK: ["3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace", "2"],
     COMBINATIONS: ["pair", "threeOfAKind", "fiveCardHands"],
-    FIVE_CARD_HANDS: ["straight", "flush", "fullHouse", "fourOfAKind", "straightFlush"]
+    FIVE_CARD_HANDS: ["straight", "flush", "fullHouse", "fourOfAKind", "straightFlush"],
+    ALL_COMBINATIONS: ["pair", "threeOfAKind", "straight", "flush", "fullHouse", "fourOfAKind", "straightFlush"]
 };
